dbf900b68b450a17b7ea3a04fc34f11c529a9d5b
R-Ahmed01 <47103663+R-Ahmed01@users.noreply.github.com>
2022-08-21 22:12:18 +0100

    sleep is refactored

--- AndroidFitness/app/src/main/java/com/example/androidfitness/datalayer/sleep/SleepDbImpl.java
+++ AndroidFitness/app/src/main/java/com/example/androidfitness/datalayer/sleep/SleepDbImpl.java
@@ -1,4 +1,4 @@
-package com.example.androidfitness.datalayer;
+package com.example.androidfitness.datalayer.sleep;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
+public class SleepDbImpl extends SQLiteOpenHelper implements SleepDb {
 
     private Context context;
     private static final String DATABASE_NAME = "AndroidFitnessSleep.db";
@@ -21,7 +21,7 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_TOTALSLEEP = "totalSleep";
     private static final String COLUMN_DEEPSLEEP = "deepSleep";
 
-    public Sleep_MyDatabaseHelper(@Nullable Context context) {
+    public SleepDbImpl(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
@@ -31,8 +31,8 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
         String query = "CREATE TABLE " + TABLE_NAME +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_DATE + " TEXT, " +
-                COLUMN_TOTALSLEEP + " INTEGER, " +
-                COLUMN_DEEPSLEEP + " INTEGER);";
+                COLUMN_TOTALSLEEP + " REAL, " +
+                COLUMN_DEEPSLEEP + " REAL);";
         db.execSQL(query);
     }
 
@@ -42,7 +42,7 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void addSleep(String date, int totalSleep, int deepSleep) {
+    public boolean addSleep(String date, float totalSleep, float deepSleep) {
         SQLiteDatabase sleep_db = this.getWritableDatabase();
         ContentValues sleep_cv = new ContentValues();
 
@@ -50,11 +50,13 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
         sleep_cv.put(COLUMN_TOTALSLEEP, totalSleep);
         sleep_cv.put(COLUMN_DEEPSLEEP, deepSleep);
         long result = sleep_db.insert(TABLE_NAME, null, sleep_cv);
-        if (result == -1) {
-            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
-        }
+        return (result == -1) ? false : true;
+
+//        if (result == -1) {
+//            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     public Cursor sleepReadAllData() {
@@ -68,7 +70,7 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
         return sleepCursor;
     }
 
-    public void updateSleepData(String row_id, String date, String totalSleep, String deepSleep) {
+    public boolean updateSleepData(String row_id, String date, String totalSleep, String deepSleep) {
         SQLiteDatabase updateSleepDB = this.getWritableDatabase();
         ContentValues sleepCV = new ContentValues();
         sleepCV.put(COLUMN_DATE, date);
@@ -76,22 +78,13 @@ public class Sleep_MyDatabaseHelper extends SQLiteOpenHelper {
         sleepCV.put(COLUMN_DEEPSLEEP, deepSleep);
 
         long updateSleepResult = updateSleepDB.update(TABLE_NAME, sleepCV, "_id=?", new String[]{row_id});
-        if (updateSleepResult == -1) {
-            Toast.makeText(context, "Failed to Update", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(context, "Updated Successfully", Toast.LENGTH_SHORT).show();
-
-        }
+        return (updateSleepResult == 0) ? false : true;
     }
 
-    public void deleteRowSleep(String row_id) {
+    public boolean deleteRowSleep(String row_id) {
         SQLiteDatabase deleteSleepDB = this.getWritableDatabase();
         long deleteResult = deleteSleepDB.delete(TABLE_NAME, "_id=?", new String[]{row_id});
-        if (deleteResult == -1) {
-            Toast.makeText(context, "Failed to delete", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(context, "Successfully Deleted", Toast.LENGTH_SHORT).show();
-        }
+        return (deleteResult == 0) ? false : true;
     }
 
     public void deleteAllSleepData() {
